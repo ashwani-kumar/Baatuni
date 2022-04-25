48b84ccbb57ae55d73784a1f66c2a7937e83d12c
ashwani-curatio <74689220+ashwani-curatio@users.noreply.github.com>
2022-04-24 23:41:40 -0700

    Initial Commit

---
+++ Baatuni/BaatuniApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaatuniApp.swift
+//  Baatuni
+//
+//  Created by Ashwani Kumar on 2022-04-24.
+//
+
+import SwiftUI
+
+@main
+struct BaatuniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
